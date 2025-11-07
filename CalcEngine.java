994fe0e49b2cfa5b186c89b3de60dfeff04ba00a
SomeWeirdMeme <ant.osuant@gmail.com>
2025-11-07 02:06:14 -0500

    Initial sharing of project

---
+++ CalcEngine.java
@@ -0,0 +1,90 @@
+/**
+ * The main part of the calculator performing the arithmetic logic of the 
+ * calculations.
+ * 
+ * @author: (none yet)
+ * @version 0.1 (incomplete)
+ */
+public class CalcEngine
+{
+    // Put instance variables here.
+    
+    /**
+     * Create a CalcEngine instance.
+     */
+    public CalcEngine()
+    {
+    }
+
+    /**
+     * Return the value of the calculator that should appear in the display right now.
+     * @return The value to be shown on the calculator display.
+     */
+    public int getDisplayValue()
+    {
+        return 0;
+    }
+
+    /**
+     * A number button was pressed. Process it.
+     * @param number The single digit.
+     */
+    public void numberPressed(int number)
+    {
+    }
+
+    /**
+     * This method should be called when the '+' button was pressed. 
+     */
+    public void plus()
+    {
+    }
+
+    /**
+     * This method should be called when the '-' button was pressed.
+     */
+    public void minus()
+    {
+    }
+    
+    /**
+     * This method should be called when the '=' button was pressed.
+     */
+    public void equals()
+    {
+    }
+
+    /**
+     * This method should be called when the 'C' (clear) button was pressed.
+     */
+    public void clear()
+    {
+    }
+
+    /**
+     * Return the name of this calculator to be shown as the title of the application.
+     * @return The title of this calculation engine.
+     */
+    public String getTitle()
+    {
+        return "";
+    }
+
+    /**
+     * Return the name of the author of this application.
+     * @return The author of this engine.
+     */
+    public String getAuthor()
+    {
+        return "";
+    }
+
+    /**
+     * Return the version number of this application.
+     * @return The version number of this engine.
+     */
+    public String getVersion()
+    {
+        return "";
+    }
+}
